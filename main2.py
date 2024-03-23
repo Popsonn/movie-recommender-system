d4f2fa15af8c29aa75c67c247e81f82e683709d0
Deborah Popoola <115980537+Popsonn@users.noreply.github.com>
2024-03-23 18:06:52 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -13,6 +13,11 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
 
 st.set_page_config(page_title="Recommender System", layout="wide")
 st.title('Movie Recommender System')
