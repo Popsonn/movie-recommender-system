6801fe1f15d5bfb193322a8d81d7503403c03b54
Deborah Popoola <115980537+Popsonn@users.noreply.github.com>
2024-03-23 16:12:29 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,7 +4,6 @@ import pickle
 import pandas as pd
 import numpy as np
 import re
-from scipy.sparse import csr_matrix
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
